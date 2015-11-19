b1d54508f14ec505526c2e35afa72286e6c3ac61
Masahiro Nagano <kazeburo@gmail.com>
2015-11-20 08:58:18 +0900

    Checking in changes prior to tagging of version 0.06.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 68e11da..7edee55 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension Redis-Jet
    
     {{$NEXT}}
    
    +0.06 2015-11-19T23:57:46Z
    +
    +    - accept args as hashref in new (thanks adokoy001)
    +
     0.05 2015-06-21T02:25:37Z
    
         - fixed test. changed error message

--- lib/Redis/Jet.pm
+++ lib/Redis/Jet.pm
@@ -14,7 +14,7 @@ our @EXPORT_OK = qw/
                     parse_message parse_message_utf8
                 /;
 
-our $VERSION = "0.05";
+our $VERSION = "0.06";
 
 use XSLoader;
 XSLoader::load(__PACKAGE__, $VERSION);
